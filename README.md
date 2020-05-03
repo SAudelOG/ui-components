b6bb36a75df2951a37860e3e99fdd32faaaf37c9
Sergio Ortiz <seortiz@expedia.com>
2020-05-03 01:08:20 -0700

    feat: rollup integration

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # UI library
 
 library of ui components to be used within react applications
+
+# Build
+The library is built with rollup
+```
+npx lerna run build
+```
--- rollup.config.factory.js
+++ rollup.config.factory.js
@@ -1,18 +1,24 @@
-export const getConfig = pkg => (
-  {
-    input: 'src/index.ts',
-    output: [
-      {
-        file: pkg.main,
-        format: 'cjs',
-      },
-      {
-        file: pkg.module,
-        format: 'es',
-      }
-    ],
-    external: [
-      ...Object.keys(pkg.peerDependencies || {})
-    ]
-  }
-);
+import typescript from 'rollup-plugin-typescript2'
+
+export const getConfig = pkg => ({
+  input: 'src/index.ts',
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs',
+    },
+    {
+      file: pkg.module,
+      format: 'es',
+    }
+  ],
+  external: [
+    ...Object.keys(pkg.dependencies || {}),
+    ...Object.keys(pkg.peerDependencies || {})
+  ],
+  plugins: [
+    typescript({
+      typescript: require('typescript'),
+    }),
+  ],
+});
