3d54d59f44e02481d33f97f96bbf76e974595694
Sergio Audel Ortiz Gutierrez <audel91@gmail.com>
2020-05-03 01:12:17 -0700

    feat: rollup integration (#1)
    * feat: adding rollup config
    
    feat: adding rollup config
    
    * feat: rollup integration
    
    Co-authored-by: Sergio Ortiz <seortiz@expedia.com>

--- packages/input/test/input.spec.tsx
+++ packages/input/test/input.spec.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { render } from "@testing-library/react";
-import { Input } from "../src"
+import { Input } from "../src/index"
 
 describe("<Input/>", () => {
     it("should render a basic component", () => {
