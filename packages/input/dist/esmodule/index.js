370ba241a6d723c18f3655692c39bee8353fc660
Sergio Ortiz <seortiz@expedia.com>
2020-05-03 02:04:02 -0700

    chore: adding linters

--- packages/input/dist/esmodule/index.js
+++
@@ -1,8 +0,0 @@
-import { createElement } from 'react';
-
-var Input = function (_a) {
-    var name = _a.name;
-    return (createElement("input", { placeholder: "type something", name: name, type: "text" }));
-};
-
-export { Input };
