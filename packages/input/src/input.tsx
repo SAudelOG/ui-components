6fd0cada48f1840cf2e1a996a99a31a5939fe6c5
Sergio Ortiz <seortiz@expedia.com>
2020-05-09 11:47:17 -0700

    feat: wiring input with react state

--- packages/input/src/input.tsx
+++ packages/input/src/input.tsx
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 interface InputProps {
     name: string;
 }
 
-export const Input: React.FC<InputProps> = ({ name }) => <input placeholder="type something" name={name} type="text" />;
+export const Input: React.FC<InputProps> = ({ name }) => {
+    const [inputValue, setInputValue] = useState('');
+
+    const onInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setInputValue(e.target.value);
+    };
+
+    return <input placeholder="type something" name={name} type="text" value={inputValue} onChange={onInputChange} />;
+};
